8e443de972cbddc51cbd09faf254f308c44bc630
MaheshMasne <maheshmasne88@gmail.com>
2023-01-13 20:38:33 +0530

    hello commit

---
+++ SmallBusinessFinance/src/app/mod/Employee/telehead/telehead.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeleheadRoutingModule } from './telehead-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TeleheadRoutingModule
+  ]
+})
+export class TeleheadModule { }
