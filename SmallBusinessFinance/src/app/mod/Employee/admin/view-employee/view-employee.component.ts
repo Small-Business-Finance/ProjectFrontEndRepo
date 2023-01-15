d099575dbd09b5a020df53461b48b34120a58ecd
preetik17 <kpreeti1703@gmail.com>
2023-01-15 14:40:53 +0530

    update and view emp component

--- SmallBusinessFinance/src/app/mod/Employee/admin/view-employee/view-employee.component.ts
+++ SmallBusinessFinance/src/app/mod/Employee/admin/view-employee/view-employee.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/app/classes/employee';
+import { EmployeeService } from 'src/app/shared/employee.service';
 
 @Component({
   selector: 'app-view-employee',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class ViewEmployeeComponent {
 
+  cp:number=1;
+  empName:any;
+
+  constructor(public cs:EmployeeService){}
+
+  e:Employee[];
+
+  ngOnInit()
+  {
+    this.cs.getAllEmployees().subscribe((employee:Employee[])=>{
+  
+      this.e=employee;
+     })
+  }
+
+  deleteEmployee(empId:number)
+  {
+    this.cs.deleteEmployee(empId).subscribe();
+    window.location.reload();
+  }
+
+  Search()
+  {
+    if(this.empName=='')
+    {
+      this.ngOnInit();
+    }
+    else{
+      this.e=this.e.filter(res=>{
+        return res.empName.toLocaleLowerCase().match(this.empName.toLocaleLowerCase());
+      });
+    }
+  }
 }
