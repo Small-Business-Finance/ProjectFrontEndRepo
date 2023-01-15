fab402bba16e354a7b2a7f347f083d5d27ef53bf
neerajvanjare <neeraj.vanjare@gmail.com>
2023-01-15 15:21:19 +0530

    component conflict error solve

--- SmallBusinessFinance/src/app/mod/Employee/re/re.module.ts
+++ SmallBusinessFinance/src/app/mod/Employee/re/re.module.ts
@@ -5,8 +5,6 @@ import { RERoutingModule } from './re-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-=======
 import { RegisterLeaveApplicationComponent } from './register-leave-application/register-leave-application.component';
 import { ViewEnquiryComponent } from './view-enquiry/view-enquiry.component';
 import { BmPersonalComponent } from '../branchmanager/bm-personal/bm-personal.component';
@@ -16,32 +14,23 @@ import { CreditanalystModule } from '../creditanalyst/creditanalyst.module';
 import { FinanceheadModule } from '../financehead/financehead.module';
 import { TeleheadModule } from '../telehead/telehead.module';
 import { OeModule } from '../oe/oe.module';
->>>>>>> fe037908e19c3e3fe7914698859243473f0718b2
 
 
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-    ProfileComponent
-=======
     ProfileComponent,
     RegisterLeaveApplicationComponent,
     ViewEnquiryComponent,
     RePersonalComponent
->>>>>>> fe037908e19c3e3fe7914698859243473f0718b2
   ],
   imports: [
     CommonModule,
     FormsModule,
-<<<<<<< HEAD
-    ReactiveFormsModule
-    RERoutingModule,RouterModule
-=======
     ReactiveFormsModule,
     RERoutingModule,RouterModule,BranchmanagerModule,CreditanalystModule,
     FinanceheadModule,TeleheadModule,OeModule
->>>>>>> fe037908e19c3e3fe7914698859243473f0718b2
+
   ]
 })
 export class REModule { }
